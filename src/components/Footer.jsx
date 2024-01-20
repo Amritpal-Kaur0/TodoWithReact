d575871e6f2244a9ad2a93910b637e2d276cd857
Amrit1-kaur <amrit.gill3005@gmail.com>
2024-01-20 20:35:02 +0530

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='relative'>
-      <p className='absolute bottom-40 text-white'>Amrit@2023 . All Right Reserved.</p>
+      <p className='fixed bottom-40 left-1/2 text-white'>Amrit@2023 . All Right Reserved.</p>
     </div>
   )
 }
